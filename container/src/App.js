247e8dc02cdfe26dbef28a44c05f0237e838c261
JDMateus <juandavidmateus@outlook.com>
2024-03-28 20:24:57 -0500

    Added a <p> to the container

--- container/src/App.js
+++ container/src/App.js
@@ -5,6 +5,7 @@ export default () => {
   return (
     <>
       <h1>Hi from the container</h1>
+      <p>This is a change from the container-dev branch</p>
       <hr />
       <MarketingApp />
     </>
